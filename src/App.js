6eb444910e1bcf364ba4f8667f9b22e3e44e8e21
dishita99 <dishita99@gmail.com>
2022-05-28 21:59:19 +0530

    Initia commit

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+import 'tachyons';
+
+
+const app = new Clarifai.App({
+ apiKey: 'ac7fa0d1255f4d6cbe86d2d520e2239e'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 150,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+       route: 'Signin',
+       isSignedIn: false
+      }
+    }
+
+
+  
+  calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  }
+}
+
+
+
+displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+
+onInputChange = (event) => {
+      this.setState({input: event.target.value});
+    }
+
+
+onButtonSubmit = () => {
+      this.setState({imageUrl: this.state.input});
+      app.models
+        .predict(
+          Clarifai.FACE_DETECT_MODEL,
+          // 'c0c0ac362b03416da06ab3fa36fb58e3',
+          this.state.input)
+         .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+         .catch(err => console.log(err));
+      }
+
+
+onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+
+render(){
+    const { isSignedIn, imageUrl, route, box} = this.state;
+    return(
+      <div className="App">
+        <Particles className='particles'
+        params={particlesOptions}
+        />
+           <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+           { route === 'home'
+           ? <div> 
+           <Logo />
+           <Rank />
+           <ImageLinkForm
+           onInputChange={this.onInputChange}
+           onButtonSubmit={this.onButtonSubmit} 
+           />
+           <FaceRecognition box={this.state.box} imageUrl={imageUrl} />
+            </div>
+            : (
+              route === 'Signin'
+              ? <Signin onRouteChange={this.onRouteChange}/>
+              : <Register onRouteChange={this.onRouteChange}/>
+            )
+        }
+
+      </div>
+    );
+  }
+  }
+
+
+export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rank = ({ name, entries }) => {
+  return (
+    <div>
+      <div className='white f1'>
+        {'MAGIC BRAIN'}
+      </div>
+      <div className='white f3'>
+        {`Hi, good to see you here!`}
+      </div>
+      
+    </div>
+  );
+}
+
+export default Rank;
